a13f47e02557da7e8032898422082bf1e41b7911
Mazbauddin <mazbauddin2010@gmail.com>
2024-07-04 14:02:46 +0600

    education add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import About from "./Components/About/About";
 import Banner from "./Components/Banner/Banner";
+import Education from "./Components/Education/Education";
+import MyProject from "./Components/MyProject/MyProject";
 import Navbar from "./Components/Navbar/Navbar";
 import Skills from "./Components/Skills/Skills";
 import Testimonial from "./Components/Testimonial/Testimonial";
@@ -20,6 +22,11 @@ const App = () => {
       <div>
         <Skills />
       </div>
+      {/* My Latest Project */}
+      <div>
+        <MyProject />
+      </div>
+      {/* Testimonial */}
       <div>
         <div className="about-title">
           <h1 className="text-red-500">Testimonial</h1>
@@ -27,6 +34,16 @@ const App = () => {
         </div>
         <div className="parallax para-2">
           <Testimonial />
+        </div>
+      </div>
+      {/* Education */}
+      <div>
+        <div className="about-title">
+          <h1 className="text-red-500">Education</h1>
+          <img src={theme_pattern} alt="" />
+        </div>
+        <div className="parallax para-2">
+          <Education />
         </div>
       </div>
     </div>
---
+++ src/Components/Education/Education.jsx
@@ -0,0 +1,14 @@
+import theme_pattern from "../../assets/theme_pattern.svg";
+
+const Education = () => {
+  return (
+    <div>
+      <div className="about-title">
+        <h1 className="text-red-500">Education</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Education;
